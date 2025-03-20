5c2ff6047b22c9adf0ad42cbfca1cd26477098b3
unknown <nishant.guvvada@gmail.com>
2025-03-20 09:45:18 +0530

    snake game completed

--- projects/snake/snake.py
+++ projects/snake/snake.py
@@ -14,12 +14,19 @@ class Snake():
 
     def create_snake(self):
         for i in range(3):
-            segment = Turtle(shape="square")
-            segment.color("white")
-            segment.penup()
-            segment.goto(x=-i*20, y=0)
-            self.segments.append(segment)
-    
+            position = (-i*20, 0)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        segment = Turtle(shape="square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
